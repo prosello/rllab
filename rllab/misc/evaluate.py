57eac4e81670e98ba70b7f92e64ecf97d6864da8
rejuvyesh <mail@rejuvyesh.com>
2016-11-14 13:59:59 -0800

    evaluate a policy

---
+++ rllab/misc/evaluate.py
@@ -0,0 +1,48 @@
+import numpy as np
+from rllab.misc import tensor_utils
+from rllab.misc import special
+from rllab.sampler.ma_sampler import cent_rollout, dec_rollout, conc_rollout
+
+
+def evaluate(env, agent, max_path_length, n_paths, mode, disc):
+    if mode == 'centralized':
+        ret = []
+        discret = []
+        envinfo = []
+        for n_path in range(n_paths):
+            path = cent_rollout(env, agent, max_path_length)
+            pathret = path['rewards'].sum()
+            pathdiscret = special.discount_cumsum(path['rewards'], disc)
+            info = path['env_infos']
+            ret.append(pathret)
+            discret.append(pathdiscret)
+            envinfo.append(info)
+
+        dictinfo = {k: np.mean(v) for k, v in tensor_utils.stack_tensor_dict_list(envinfo).items()}
+        return dict(ret=np.mean(ret), discret=np.mean(discret), **dictinfo)
+
+    elif mode == 'decentralized':
+        agent2paths = {}
+        for agid in range(len(env.agents)):
+            agent2paths[agid] = []
+
+        for n_path in range(n_paths):
+            paths = dec_rollout(env, agent, max_path_length)
+            for agid, agpath in enumerate(paths):
+                agent2paths[agid].append(agpath)
+
+        rets, retsstd, discrets, infos = [], [], [], []
+        for agid, paths in agent2paths.items():
+            rets.append(np.mean([path['rewards'].sum() for path in paths]))
+            retsstd.append(np.std([path['rewards'].sum() for path in paths]))
+            discrets.append(
+                np.mean([special.discount_cumsum(path['rewards'], disc)[0] for path in paths]))
+            infos.append({
+                k: np.mean(v)
+                for k, v in tensor_utils.stack_tensor_dict_list(
+                    [path['env_infos'] for path in paths]).items()
+            })
+            dictinfos = tensor_utils.stack_tensor_dict_list(infos)
+        return dict(ret=rets, retstd=retsstd, discret=discrets, **dictinfos)
+    elif mode == 'concurrent':
+        raise NotImplementedError()
